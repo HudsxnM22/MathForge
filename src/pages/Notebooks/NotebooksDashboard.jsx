3f2030bbd0eb9b736c0cf13c4c53545387212e23
HudsxnM22 <maioranahudson@gmail.com>
2025-03-24 13:19:39 -0400

    updated notebooks dashboard

--- src/pages/Notebooks/NotebooksDashboard.jsx
+++ src/pages/Notebooks/NotebooksDashboard.jsx
@@ -3,23 +3,27 @@ import styles from './NotebooksDashboard.module.css';
 import useUserStore from '../../hooks/useUserStore';
 import { Navigate } from 'react-router-dom';
 
+//for context for what a notebook is. its a collection of 5 math problems and its fetched from the backend and stored in the database
+//the user can create a notebook and add problems to it. the user can also delete a notebook and delete problems from it
 
 const NotebooksDashboard = () => {
     const toggleUserLogIn = useUserStore(state => state.toggleLogIn);
     const user = useUserStore(state => state.user);
 
     return (
-        <>
+        <div className={styles.notebookDashboardContainer}>
             {user.isLoggedIn ?
-                <div>
-                    <h1>Your NoteBooks</h1>
-                    <p>Create Notebooks</p>
-                    <button onClick={toggleUserLogIn}>Test Login UI</button>
-                </div>
+                <section className={styles.notebooksDashboard}>
+                    <div className={styles.notebookHolderContainer}>
+                        <div className={styles.notebookHolder}>
+
+                        </div>
+                    </div>
+                </section>
             :
                 <Navigate to="/" />
             }
-        </>
+        </div>
     );
 };
 //implement on logout redirect to home
